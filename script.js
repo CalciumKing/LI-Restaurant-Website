824572800bb3ff46392651230b2f635d7be01a4c
CalciumKing <129754936+CalciumKing@users.noreply.github.com>
2024-11-19 14:12:10 -0500

    Basic styling and side bar
    basic styling and created a side nav bar with a functioning burger menu, there are several different animations that go along with opening and closing the side nav menu

---
+++ script.js
@@ -0,0 +1,11 @@
+let navOpen = false;
+function toggleNav() {
+    let sidebar = document.getElementById("sidebar");
+    if(!navOpen) {
+        sidebar.style.width = "250px";
+        navOpen = true;
+    } else {
+        sidebar.style.width = "0";
+        navOpen = false;
+    }
+}
