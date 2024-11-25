604349c9529edcb5549ab760623b772d5a0f1764
CalciumKing <129754936+CalciumKing@users.noreply.github.com>
2024-11-25 14:18:16 -0500

    Format slideshow, footer, svg files
    In the regular web all I did was format the slideshow, add footer information, import all the logos as svg files, and slightly add them in using css.

--- script.js
+++ script.js
@@ -1,4 +1,10 @@
 let navOpen = false;
+let slideIndex = 1;
+
+!function () {
+    showSlides(slideIndex);
+}();
+
 function toggleNav() {
     let sidebar = document.getElementById("sidebar");
     if (!navOpen) {
@@ -8,4 +14,33 @@ function toggleNav() {
         sidebar.style.width = "0";
         navOpen = false;
     }
+}
+
+function plusSlides(n) {
+    showSlides(slideIndex += n);
+}
+
+function setSlide(n) {
+    showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+    let slides = document.getElementsByClassName("slide");
+    let dots = document.getElementsByClassName("dot");
+
+    if (n > slides.length) {
+        slideIndex = 1
+    } else if (n < 1) {
+        slideIndex = slides.length
+    }
+
+    for (let i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    for (let i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+
+    slides[slideIndex - 1].style.display = "block";
+    dots[slideIndex - 1].className += " active";
 }
