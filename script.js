8294a52026edcd1f65ed6df22273ca37904ce62b
CalciumKing <129754936+CalciumKing@users.noreply.github.com>
2024-11-26 14:14:41 -0500

    Lots of changes, lots of functionality and javascript
    Working on adding information to the html page, I added images, and a footer, I also changed how I was doing logos that go on the same line as text, I was using css to make a span have a background image that was the logo, but instead I changed it to an image tag that has display: inline on it. I also managed to add code that would change the link to maroon on hover no matter what color it is using filter css. I made the slideshow functional and works both automatically and manually, it constantly changes its image but when you hover over it the image gets bigger and the slideshow stops, something I always want slideshows to do but they don't. Lastly I added things I add to every site I make, a horizontal scroll bar on the top of the page and a to top button. I added lots of javascript for most of the things I mentioned.

--- script.js
+++ script.js
@@ -1,14 +1,39 @@
 let navOpen = false;
 let slideIndex = 1;
+let timeoutID;
+const slideWait = 2000;
 
 !function () {
+    window.addEventListener("scroll", () => document.getElementById("toTopButton").style.display = (window.scrollY > 0) ? "block" : "none");
+    for (const image of document.getElementsByClassName("slideImage")) {
+        image.addEventListener("mouseover", () => clearTimeout(timeoutID));
+        image.addEventListener("mouseout", () => {
+            clearTimeout(timeoutID);
+            timeoutID = setTimeout(() => {
+                slideIndex++;
+                showSlides(slideIndex);
+            }, slideWait);
+        });
+    }
+    
+    scrollProgressBar();
     showSlides(slideIndex);
 }();
+
+function scrollProgressBar() {
+    const getTabHeight = () => document.documentElement.scrollHeight - window.innerHeight;
+    const getWidth = () => {
+        let totalHeight = getTabHeight();
+        if (totalHeight === 0) return "100%";
+        return `${(window.scrollY / totalHeight) * 100}%`;
+    };
+    document.addEventListener("scroll", () => document.getElementById("progressBar").style.width = getWidth());
+}
 
 function toggleNav() {
     let sidebar = document.getElementById("sidebar");
     if (!navOpen) {
-        sidebar.style.width = "250px";
+        sidebar.style.width = "15vw";
         navOpen = true;
     } else {
         sidebar.style.width = "0";
@@ -17,10 +42,12 @@ function toggleNav() {
 }
 
 function plusSlides(n) {
+    clearTimeout(timeoutID);
     showSlides(slideIndex += n);
 }
 
 function setSlide(n) {
+    clearTimeout(timeoutID);
     showSlides(slideIndex = n);
 }
 
@@ -29,9 +56,9 @@ function showSlides(n) {
     let dots = document.getElementsByClassName("dot");
 
     if (n > slides.length) {
-        slideIndex = 1
+        slideIndex = 1;
     } else if (n < 1) {
-        slideIndex = slides.length
+        slideIndex = slides.length;
     }
 
     for (let i = 0; i < slides.length; i++) {
@@ -43,4 +70,9 @@ function showSlides(n) {
 
     slides[slideIndex - 1].style.display = "block";
     dots[slideIndex - 1].className += " active";
+
+    timeoutID = setTimeout(() => {
+        slideIndex++;
+        showSlides(slideIndex);
+    }, slideWait);
 }
