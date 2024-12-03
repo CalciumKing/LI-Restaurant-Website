701a2a30748a7d7e51167a649cd88362c3653838
CalciumKing <129754936+CalciumKing@users.noreply.github.com>
2024-12-03 07:40:13 -0500

    improving code
    reviewing code and improving it, shortening or making it more efficient.

--- script.js
+++ script.js
@@ -1,10 +1,23 @@
+const slideWait = 2000;
+const sidebar = document.getElementById("sidebar");
+const progressBar = document.getElementById("progressBar");
+const slides = document.getElementsByClassName("slide");
+const dots = document.getElementsByClassName("dot");
+const topButton = document.getElementById("toTopButton");
+
 let navOpen = false;
 let slideIndex = 1;
 let timeoutID;
-const slideWait = 2000;
 
-!function () {
-    window.addEventListener("scroll", () => document.getElementById("toTopButton").style.display = (window.scrollY > 0) ? "block" : "none");
+!function() {
+    window.addEventListener("scroll", () => {
+        topButton.style.display = (window.scrollY > 0) ? "block" : "none";
+        const getTabHeight = () => document.documentElement.scrollHeight - window.innerHeight;
+        progressBar.style.width = `${(window.scrollY / getTabHeight()) * 100}%`;
+    });
+
+    topButton.addEventListener("click", () => window.scrollTo({top:0, behavior:"smooth"}));
+
     for (const image of document.getElementsByClassName("slideImage")) {
         image.addEventListener("mouseover", () => clearTimeout(timeoutID));
         image.addEventListener("mouseout", () => {
@@ -15,30 +28,13 @@ const slideWait = 2000;
             }, slideWait);
         });
     }
-    
-    scrollProgressBar();
+
     showSlides(slideIndex);
 }();
 
-function scrollProgressBar() {
-    const getTabHeight = () => document.documentElement.scrollHeight - window.innerHeight;
-    const getWidth = () => {
-        let totalHeight = getTabHeight();
-        if (totalHeight === 0) return "100%";
-        return `${(window.scrollY / totalHeight) * 100}%`;
-    };
-    document.addEventListener("scroll", () => document.getElementById("progressBar").style.width = getWidth());
-}
-
 function toggleNav() {
-    let sidebar = document.getElementById("sidebar");
-    if (!navOpen) {
-        sidebar.style.width = "15vw";
-        navOpen = true;
-    } else {
-        sidebar.style.width = "0";
-        navOpen = false;
-    }
+    sidebar.style.width = (navOpen) ? 0 : "15vw";
+    navOpen = !navOpen;
 }
 
 function plusSlides(n) {
@@ -52,21 +48,11 @@ function setSlide(n) {
 }
 
 function showSlides(n) {
-    let slides = document.getElementsByClassName("slide");
-    let dots = document.getElementsByClassName("dot");
+    if (n > slides.length) slideIndex = 1;
+    else if (n < 1) slideIndex = slides.length;
 
-    if (n > slides.length) {
-        slideIndex = 1;
-    } else if (n < 1) {
-        slideIndex = slides.length;
-    }
-
-    for (let i = 0; i < slides.length; i++) {
-        slides[i].style.display = "none";
-    }
-    for (let i = 0; i < dots.length; i++) {
-        dots[i].className = dots[i].className.replace(" active", "");
-    }
+    for (let i = 0; i < slides.length; i++) slides[i].style.display = "none";
+    for (let i = 0; i < dots.length; i++) dots[i].className = dots[i].className.replace(" active", "");
 
     slides[slideIndex - 1].style.display = "block";
     dots[slideIndex - 1].className += " active";
